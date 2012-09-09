aea5656226d04e1a0ea8704639fa66cc091a83b2
David Gullmarsvik <awt2.2012@gmail.com>
2012-09-09 20:52:19 +0200

    Uppgift 7

---
+++ Lektion10.Tests/ProductControllerTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lektion10.Model.Entities;
+using Moq;
+using Lektion10.Model.Abstract;
+using Lektion10.Web.Controllers;
+
+namespace Lektion10.Tests
+{
+    [TestClass]
+    public class ProductControllerTests
+    {
+        [TestMethod]
+        public void ProductController_Edit_CanSaveValidProduct()
+        {
+            //Arrange
+            Product p = new Product { ProductID = 1, Name = "Test" };
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            var target = new ProductController(mock.Object);
+
+            //Act
+            var result = target.Edit(p);
+
+            //Assert
+            mock.Verify(r => r.Save(p), Times.Once());
+        }
+
+        [TestMethod]
+        public void ProductController_Edit_CannotSaveInvalidProduct()
+        {
+            //Arrange
+            Product p = new Product { ProductID = 1, Name = "Test" };
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            var target = new ProductController(mock.Object);
+            target.ModelState.AddModelError("error", "error");
+
+            //Act
+            var result = target.Edit(p);
+
+            //Assert
+            mock.Verify(r => r.Save(It.IsAny<Product>()), Times.Never());
+        }
+    }
+}
